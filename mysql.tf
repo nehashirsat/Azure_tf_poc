6749806cad330422ec178aa85dcdc8f333ba9c3a
nehashirsat <50192586+nehashirsat@users.noreply.github.com>
2021-10-13 10:15:27 +0530

    Update mysql.tf

--- mysql.tf
+++ mysql.tf
@@ -14,7 +14,7 @@ resource "azurerm_mysql_server" "mysql_server" {
   backup_retention_days             = 7
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
+  public_network_access_enabled     = false
   ssl_enforcement_enabled           = false
   #ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
