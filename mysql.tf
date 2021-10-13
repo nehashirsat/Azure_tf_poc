3bd9d71c185ab1537b6c05f2cb538d8aa3024918
nehashirsat <50192586+nehashirsat@users.noreply.github.com>
2021-10-13 10:28:20 +0530

    Update mysql.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- mysql.tf
+++ mysql.tf
@@ -17,6 +17,9 @@ resource "azurerm_mysql_server" "mysql_server" {
   public_network_access_enabled     = false
   ssl_enforcement_enabled           = true
   #ssl_minimal_tls_version_enforced  = "TLS1_2"
+  threat_detection_policy {
+    enabled = true
+  }
 }
 
 resource "azurerm_mysql_firewall_rule" "firewall_rule" {
