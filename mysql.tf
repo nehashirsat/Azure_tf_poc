ff409937a61be29c5ec8a1640846f71d732be915
nehashirsat <50192586+nehashirsat@users.noreply.github.com>
2021-10-13 10:28:09 +0530

    Update mysql.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- mysql.tf
+++ mysql.tf
@@ -15,7 +15,7 @@ resource "azurerm_mysql_server" "mysql_server" {
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = false
-  ssl_enforcement_enabled           = false
+  ssl_enforcement_enabled           = true
   #ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
